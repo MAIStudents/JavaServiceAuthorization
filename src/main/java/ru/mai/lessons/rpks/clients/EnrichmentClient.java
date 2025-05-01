96be50734d49b1595d814499e176b80d58f0a58a
Krolicc <147572580+Krolicc@users.noreply.github.com>
2025-05-01 19:57:52 +0300

    Add clients files

--- src/main/java/ru/mai/lessons/rpks/clients/EnrichmentClient.java
+++ src/main/java/ru/mai/lessons/rpks/clients/EnrichmentClient.java
@@ -1,6 +1,10 @@
 package ru.mai.lessons.rpks.clients;
 
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+import ru.mai.lessons.rpks.dto.request.EnrichmentRequest;
+import ru.mai.lessons.rpks.dto.response.EnrichmentResponse;
 
 /**
  * Клиент для контролера обогащения
@@ -10,5 +14,29 @@ import org.springframework.cloud.openfeign.FeignClient;
     url = "${feign.client.url.enrichment}"
 )
 public interface EnrichmentClient {
-  //TODO code here...
+    @GetMapping("/findAll")
+    Iterable<EnrichmentResponse> getAllEnrichmentRequests();
+
+    @GetMapping("/findAll/{id}")
+    Iterable<EnrichmentResponse> getAllEnrichmentRequestsByEnrichmentRequestId(
+            @PathVariable("id") long id
+    );
+
+    @GetMapping("/find/{enrichmentId}/{ruleId}")
+    EnrichmentResponse getEnrichmentRequestById(
+            @PathVariable("enrichmentId") long enrichmentId,
+            @PathVariable("ruleId") long ruleId
+    );
+
+    @DeleteMapping("/delete")
+    void deleteEnrichmentRequest();
+
+    @DeleteMapping("/delete/{enrichmentId}/{ruleId}")
+    void deleteEnrichmentRequestById(
+            @PathVariable("enrichmentId") long enrichmentId,
+            @PathVariable("ruleId") long ruleId
+    );
+
+    @PostMapping("/save")
+    void save(@RequestBody @Valid EnrichmentRequest enrichment);
 }
