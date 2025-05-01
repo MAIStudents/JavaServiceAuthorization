a23eba2819d25a8bca4678d11c16424cf155a595
Krolicc <147572580+Krolicc@users.noreply.github.com>
2025-05-01 20:00:38 +0300

    Upload implemented service files

--- src/main/java/ru/mai/lessons/rpks/services/impl/RegisterServiceImpl.java
+++ src/main/java/ru/mai/lessons/rpks/services/impl/RegisterServiceImpl.java
@@ -1,29 +1,55 @@
 package ru.mai.lessons.rpks.services.impl;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.mai.lessons.rpks.dto.response.TokenResponse;
+import ru.mai.lessons.rpks.models.User;
+import ru.mai.lessons.rpks.repositories.UserRepository;
 import ru.mai.lessons.rpks.services.RegisterService;
+import ru.mai.lessons.rpks.services.UserService;
 
+import java.time.Duration;
+import java.util.Date;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RegisterServiceImpl implements RegisterService {
 
-  //TODO inject UserService...
+  private final UserService userService;
+  private final UserRepository userRepository;
 
-  //TODO inject token.signing.secret value...
   @Value("${token.signing.secret}")
   private String secret;
 
-  //TODO inject token.signing.issuer value...
   @Value("${token.issuer}")
   private String issuer;
 
+  @Value("${token.duration}")
+  private Duration duration;
+
   @Override
   public TokenResponse register(String username) {
-    //TODO generate token by JWT with issuer, subject, issuedAt, expiresAt and sign by HMAC256...
-    //TODO create user...
-    return new TokenResponse();
+
+    if (userRepository.findByUsername(username).isPresent()) {
+      log.warn("User has been registered");
+      return new TokenResponse();
+    }
+
+    userService.createUser(User.builder().username(username).build());
+
+    String token = JWT.create()
+            .withIssuer(issuer)
+            .withSubject(username)
+            .withIssuedAt(new Date())
+            .withExpiresAt(new Date(System.currentTimeMillis() + duration.toMillis()))
+            .sign(Algorithm.HMAC256(secret));
+
+
+    return new TokenResponse(token);
   }
 }
